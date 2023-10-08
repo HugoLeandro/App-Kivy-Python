f3007e8a36afe17ffe7ed2f2295e73db4201b78c
siteflask1 <leandrolimahugo@gmail.com>
2023-10-08 08:10:56 -0300

    update design

--- bannervenda.py
+++ bannervenda.py
@@ -2,13 +2,19 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
-from kivy.graphics import Rectangle, Canvas
+from kivy.graphics import Rectangle, Color
 
 
 class BannerVenda(GridLayout):
 
     def __init__(self, **kwargs):
+        self.rows = 1
         super().__init__()
+
+        with self.canvas:
+            Color(rgb=(0, 0, 0, 1))
+            self.rec = Rectangle(size=self.size, pos=self.pos)
+        self.bind(pos=self.atualizar_rec, size=self.atualizar_rec)
 
         # kwargs = {"cliente": "mundial", "foto_cliente": "mundial.png"}
 
@@ -18,7 +24,7 @@ class BannerVenda(GridLayout):
         foto_produto = kwargs['foto_produto']
         data = kwargs['data']
         unidade = kwargs['unidade']
-        quantidate = kwargs['quantidade']
+        quantidade = kwargs['quantidade']
         preco = kwargs['preco']
 
         esquerda = FloatLayout()
@@ -28,12 +34,26 @@ class BannerVenda(GridLayout):
         esquerda.add_widget(esquerda_imagem)
         esquerda.add_widget(esquerda_label)
 
-
-
         meio = FloatLayout()
+        meio_imagem = Image(pos_hint={"right": 1, "top": 0.95}, size_hint=(1, 0.75),
+                                source=f"icones/fotos_produtos/{foto_produto}")
+        meio_label = Label(text=produto, size_hint=(1, 0.2), pos_hint= {"right": 1, "top": 0.2})
+        meio.add_widget(meio_imagem)
+        meio.add_widget(meio_label)
 
         direita = FloatLayout()
+        direita_label_data = Label(text=f"Data: {data}", size_hint=(1, 0.33), pos_hint={"right":1, "top": 0.9})
+        direita_label_preco = Label(text=f"Preço: R${preco:,.2f}", size_hint=(1, 0.33), pos_hint={"right": 1, "top": 0.65})
+        direita_label_quantidade = Label(text=f"{quantidade} {unidade}", size_hint=(1, 0.33), pos_hint={"right": 1, "top": 0.4})
+        direita.add_widget(direita_label_data)
+        direita.add_widget(direita_label_preco)
+        direita.add_widget(direita_label_quantidade)
+
 
         self.add_widget(esquerda)
         self.add_widget(meio)
         self.add_widget(direita)
+
+    def atualizar_rec(self, *args):
+        self.rec.pos = self.pos
+        self.rec.size = self.size
